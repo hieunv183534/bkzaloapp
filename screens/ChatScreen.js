5041eb70b80ece55a0d8365f3b9155e373241d4b
Đặng Huỳnh Đức <ducdang@wisami.com>
2022-01-09 14:03:50 +0700

    Display messages sended

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -4,6 +4,7 @@ import { Bubble, GiftedChat, Send } from 'react-native-gifted-chat';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import ChatSerrvice from '../src/api/ChatService';
+import ConversationApi from '../src/api/ConversationApi';
 
 const ChatScreen = (route) => {
   const [messages, setMessages] = useState([]);
@@ -19,13 +20,53 @@ const ChatScreen = (route) => {
   const [connectionState, setConnectedStateText] = useState('');
   const [isConnected, setConnected] = useState(false);
 
-  useEffect(() => {
+  useEffect(async () => {
     const { userName, phone, token, receiveID, conversationId } = route.route.params;
     onUserName(userName);
     onPhone(phone);
     onToken(token);
     onReceiveID(receiveID);
     console.log("conversationId: ", conversationId);
+    console.log("userName: ", userName);
+
+    const conversationApi = new ConversationApi(token);
+    await conversationApi
+      .getConversation(conversationId, 0, 10)
+      .then((res) => {
+        console.log(res);
+        if (res.data.code === 1000) {
+          let messages = [];
+          const mess = res.data.data.messages;
+          for (let i in mess) {
+            let nhan = 0;
+            let gui = 0;
+            if (receiveID === mess[i].sender.accountId) {
+              gui = 2;
+              nhan = 1;
+            }
+            else {
+              gui = 1;
+              nhan = 2;
+            }
+            const curr = {
+              _id: nhan,
+              text: mess[i].content,
+              createdAt: mess[i].createdAt,
+              user: {
+                _id: gui,
+                name: mess[i].sender.userName,
+                avatar: 'https://placeimg.com/140/140/any',
+              },
+
+            };
+            messages.push(curr)
+          }
+          setMessages(messages)
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
     // onChatServer(new ChatSerrvice(token.split(' ')[1]));
     // const abc = new ChatSerrvice(token.split(' ')[1]);
     // console.log('abc: ', abc);
@@ -38,30 +79,50 @@ const ChatScreen = (route) => {
     // });
   }, []);
 
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello developer ABC',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-      {
-        _id: 2,
-        text: 'Hello world',
-        createdAt: new Date(),
-        user: {
-          _id: 1,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-    ]);
-  }, []);
+  // useEffect(() => {
+  //   setMessages([
+  //     {
+  //       _id: 1,
+  //       text: 'Hello developer ABC',
+  //       createdAt: new Date(),
+  //       user: {
+  //         _id: 2,
+  //         name: 'React Native',
+  //         avatar: 'https://placeimg.com/140/140/any',
+  //       },
+  //     },
+  //     {
+  //       _id: 4,
+  //       text: 'Hello world',
+  //       createdAt: new Date(),
+  //       user: {
+  //         _id: 1,
+  //         name: 'React Native',
+  //         avatar: 'https://placeimg.com/140/140/any',
+  //       },
+  //     },
+  //     {
+  //       _id: 4,
+  //       text: 'yyyyyyyyyyyyyyyyyyyy',
+  //       createdAt: new Date(),
+  //       user: {
+  //         _id: 1,
+  //         name: 'React Native',
+  //         avatar: 'https://placeimg.com/140/140/any',
+  //       },
+  //     },
+  //     {
+  //       _id: 1,
+  //       text: 'aaaaaaaaaaaaaaaaa',
+  //       createdAt: new Date(),
+  //       user: {
+  //         _id: 4,
+  //         name: 'React Native',
+  //         avatar: 'https://placeimg.com/140/140/any',
+  //       },
+  //     },
+  //   ]);
+  // }, []);
 
   // const onSend = (messages) => {
   //   console.log('mess: ', messages);
